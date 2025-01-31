236b1b7cc1b295983da1c7786e51febb164af9b5
Devanshi Jhawar <89416589+devanshi-j@users.noreply.github.com>
2025-01-31 18:04:18 +0530

    Update main.js

--- complete/main.js
+++ complete/main.js
@@ -33,20 +33,20 @@ const deepClone = (obj) => {
 };
 
 const itemCategories = {
-    lamp: [
-        { name: "lamp1", height: 0.3 },
-        { name: "lamp2", height: 0.35 },
-        { name: "lamp3", height: 0.28 }
-    ],
-    sofa: [
-        { name: "sofa1", height: 0.1 },
-        { name: "sofa2", height: 0.12 },
-        { name: "sofa3", height: 0.15 }
-    ],
     table: [
-        { name: "table1", height: 0.2 },
-        { name: "table2", height: 0.25 },
-        { name: "table3", height: 0.22 }
+        { name: "table1", height: 0.3 },
+        { name: "table2", height: 0.35 },
+        { name: "table3", height: 0.28 }
+    ],
+    chair: [
+        { name: "chair1", height: 0.1 },
+        { name: "chair2", height: 0.12 },
+        { name: "chair3", height: 0.15 }
+    ],
+    shelf: [
+        { name: "shelf1", height: 0.2 },
+        { name: "shelf2", height: 0.25 },
+        { name: "shelf3", height: 0.22 }
     ]
 };
 
